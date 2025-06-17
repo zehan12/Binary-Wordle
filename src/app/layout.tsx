65a23bb67dd2444a5594ce4d44141848674e7de8
zehan12 <zehan9211@gmail.com>
2025-06-18 02:15:54 +0530

    add shadcn ui libary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
+      <body suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
